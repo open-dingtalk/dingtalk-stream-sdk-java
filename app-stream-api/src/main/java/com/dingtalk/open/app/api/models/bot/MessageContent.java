6294aa68936c7d8a55ff11f5a4e779239b135cab
Ke Jie <chzealot@gmail.com>
2023-09-21 19:31:02 +0800

    feat: 为 MessageContent 字段补充 getter 和 setter
    Signed-off-by: Ke Jie <chzealot@gmail.com>

--- app-stream-api/src/main/java/com/dingtalk/open/app/api/models/bot/MessageContent.java
+++ app-stream-api/src/main/java/com/dingtalk/open/app/api/models/bot/MessageContent.java
@@ -57,4 +57,108 @@ public class MessageContent implements Serializable {
      * 适用的类型：unknownMsgType
      */
     String unknownMsgType;
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPictureDownloadCode() {
+        return pictureDownloadCode;
+    }
+
+    public void setPictureDownloadCode(String pictureDownloadCode) {
+        this.pictureDownloadCode = pictureDownloadCode;
+    }
+
+    public String getDownloadCode() {
+        return downloadCode;
+    }
+
+    public void setDownloadCode(String downloadCode) {
+        this.downloadCode = downloadCode;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public String getVideoType() {
+        return videoType;
+    }
+
+    public void setVideoType(String videoType) {
+        this.videoType = videoType;
+    }
+
+    public String getRecognition() {
+        return recognition;
+    }
+
+    public void setRecognition(String recognition) {
+        this.recognition = recognition;
+    }
+
+    public String getSpaceId() {
+        return spaceId;
+    }
+
+    public void setSpaceId(String spaceId) {
+        this.spaceId = spaceId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public List<MessageContent> getRichText() {
+        return richText;
+    }
+
+    public void setRichText(List<MessageContent> richText) {
+        this.richText = richText;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUnknownMsgType() {
+        return unknownMsgType;
+    }
+
+    public void setUnknownMsgType(String unknownMsgType) {
+        this.unknownMsgType = unknownMsgType;
+    }
 }
