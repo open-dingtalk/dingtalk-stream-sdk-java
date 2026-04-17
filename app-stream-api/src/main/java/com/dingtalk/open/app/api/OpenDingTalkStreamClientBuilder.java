d050588a8074a04e5b7ae447c22d9ee95c4a6c91
xize.zx <xize.zx@alibaba-inc.com>
2026-04-17 11:35:42 +0800

    feat: add oversea region support for DingTalk Stream SDK
    Add oversea() and overseaPreEnv() methods to OpenDingTalkStreamClientBuilder, allowing users to switch API endpoint to https://api.dingtalk.io and https://pre-api.dingtalk.io for overseas DingTalk users.

--- app-stream-api/src/main/java/com/dingtalk/open/app/api/OpenDingTalkStreamClientBuilder.java
+++ app-stream-api/src/main/java/com/dingtalk/open/app/api/OpenDingTalkStreamClientBuilder.java
@@ -106,6 +106,30 @@ public class OpenDingTalkStreamClientBuilder {
     }
 
     /**
+     * 使用海外版钉钉正式环境
+     * <p>
+     * 海外纯血版钉钉用户请使用此方法，API地址将切换为 https://api.dingtalk.io
+     * </p>
+     *
+     * @return builder
+     */
+    public OpenDingTalkStreamClientBuilder oversea() {
+        return this.openApiHost("https://api.dingtalk.io");
+    }
+
+    /**
+     * 使用海外版钉钉预发环境
+     * <p>
+     * 海外纯血版钉钉用户预发环境，API地址将切换为 https://pre-api.dingtalk.io
+     * </p>
+     *
+     * @return builder
+     */
+    public OpenDingTalkStreamClientBuilder overseaPreEnv() {
+        return this.openApiHost("https://pre-api.dingtalk.io");
+    }
+
+    /**
      * 设置代理方式
      *
      * @param netProxy
